af6860ed7f80ddab43351ebeab6119122add2c09
Elena Serykh <elena.serykh@waveaccess.ru>
2021-11-09 12:24:01 +0300

    README was modified

---
+++ pyguides/__main__.py
@@ -0,0 +1,73 @@
+# type: ignore[attr-defined]
+from typing import Optional
+
+import logging
+from enum import Enum
+from random import choice
+
+import typer
+from rich.console import Console
+
+from pyguides import __version__
+from pyguides.example import hello
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyguides").setLevel(logging.DEBUG)
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
+
+app = typer.Typer(
+    name="PYGuides",
+    help="Usefull guides to deal with Python3 ",
+    add_completion=False,
+)
+console = Console()
+
+
+def version_callback(print_version: bool) -> None:
+    """Print the version of the package."""
+    if print_version:
+        console.print(f"[yellow]PYGuides[/] version: [bold blue]{__version__}[/]")
+        raise typer.Exit()
+
+
+@app.command(name="")
+def main(
+    name: str = typer.Option(..., help="Person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        "-c",
+        "--color",
+        "--colour",
+        case_sensitive=False,
+        help="Color for print. If not specified then choice will be random.",
+    ),
+    print_version: bool = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the PYGuides package.",
+    ),
+) -> None:
+    """Print a greeting with a giving name."""
+    if color is None:
+        color = choice(list(Color))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
+
+
+if __name__ == "__main__":
+    app()
